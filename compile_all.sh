7f3bb181e322437072c5c34415ed0dbf46d663a4
忘忧北萱草 <1779724477@qq.com>
2018-07-17 19:40:08 +0800

    sh-make 02

--- compile_all.sh
+++ compile_all.sh
@@ -5,15 +5,15 @@ function buildtools(){
 }
 
 function buildprograms(){
-    local outs = `echo $@| cut -d ' ' -f 3-`
+    local outs=`echo $@| cut -d ' ' -f 3-`
     gcc -g $1 -o $2 $outs -stdc++
 }
 
 # 第一步：编译 tools 文件夹
-let tools_changed = 0
+tools_changed=0
 for source in $(find 'tools/' -name '*.cpp'); do
-    header = `echo $source | awk 'gsub(".cpp",".hpp"){print $0}'`
-    output = `echo $source | awk 'gsub(".cpp",".out"){print $0}'`
+    header=`echo $source | awk 'gsub(".cpp",".hpp"){print $0}'`
+    output=`echo $source | awk 'gsub(".cpp",".out"){print $0}'`
     if [ $source -ot $output ]; then # 如果源代码在上一次编译之后没有更改
         if [ $header -ot $output ]; then # 如果头文件在上一次编译之后没有更改
             continue
@@ -22,14 +22,14 @@ for source in $(find 'tools/' -name '*.cpp'); do
     # 如果输出文件不存在，或者源代码或头文件有变动，重新编译
     buildtools $source $output
     echo Compile: $source -\> $output
-    let tools_changed = 1
+    tools_changed=1
 done
 
 # 第二步：编译各个程序
 folders = `ls -F | grep [0-9]`
 for folder in $folders; do
-    for source in $(find $folders -name '*.cpp'`); do
-        output = `echo $source | awk 'gsub(".cpp",".cpp.exe"){print $0}'`
+    for source in $(find $folders -name '*.cpp'); do
+        output=`echo $source | awk 'gsub(".cpp",".cpp.exe"){print $0}'`
         if [ $tools_changed -eq 0 ]; then
             if [ $source -ot $output ]; then # 如果源代码在上一次编译之后没有更改
                     continue
